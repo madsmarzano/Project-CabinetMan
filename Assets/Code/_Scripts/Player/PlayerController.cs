38c73b7d9fdb9f81096e843008fc88fbd7e5205b
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-09-30 22:41:25 -0400

    Revised State Machine

--- Assets/Code/_Scripts/Player/PlayerController.cs
+++ Assets/Code/_Scripts/Player/PlayerController.cs
@@ -1,13 +1,9 @@
+using UnityEngine;
+
 namespace CabinetMan.Player
 {
-    using CabinetMan.Player.StateMachine;
-    using CabinetMan.Player.States;
-    using UnityEngine;
-
     public class PlayerController : MonoBehaviour
     {
-        [HideInInspector]
-        public PlayerStateMachine PSM;
         [HideInInspector]
         public InputHandler input;
         [HideInInspector]
@@ -21,11 +17,22 @@ namespace CabinetMan.Player
         public PlayerData humanSizeData;
         public PlayerData bugSizeData;
 
-        [HideInInspector]
-        public PlayerIdleState idleState;
-        public PlayerMovingState movingState;
-        public PlayerJumpState jumpState;
-        public PlayerInAirState inAirState;
+        public float xInputRaw;
+        public float zInputRaw;
+
+        public float jumpTimer = 0;
+        public bool canJump = true;
+
+        public enum PlayerState
+        {
+            IDLE,
+            MOVING, //Grounded
+            JUMPING,
+            FALLING, //In the air but not jumping
+            LANDED, //Resets flightTimer and jumpCount, then transitions to a grounded state like IDLE or MOVING.
+        }
+
+        public PlayerState currentState;
 
         public bool isGrounded;
         public LayerMask ground;
@@ -34,15 +41,9 @@ namespace CabinetMan.Player
 
         private void Awake()
         {
-            PSM = new PlayerStateMachine();
             input = GetComponent<InputHandler>();
             rb = GetComponent<Rigidbody>();
             cc = GetComponent<CapsuleCollider>();
-
-            idleState = new PlayerIdleState(this);
-            movingState = new PlayerMovingState(this);
-            jumpState = new PlayerJumpState(this);
-            inAirState = new PlayerInAirState(this);
 
             ground = LayerMask.GetMask("Ground");
 
@@ -52,12 +53,27 @@ namespace CabinetMan.Player
 
         private void Start()
         {
-            PSM.Initialize(idleState);
+            currentState = PlayerState.IDLE;
+
+            data.jumpCount = 0;
         }
 
         private void Update()
         {
-            PSM.currentState.Update();
+            //switch statement that handles transitions between states
+            switch (currentState)
+            {
+                case PlayerState.IDLE:
+                    IdleUpdate(); break;
+                case PlayerState.MOVING:
+                    MovingUpdate(); break;
+                case PlayerState.JUMPING:
+                    JumpingUpdate(); break;
+                case PlayerState.FALLING:
+                    FallingUpdate(); break;
+                case PlayerState.LANDED:
+                    LandedUpdate(); break;
+            }
 
             //Probably should move this into one of the states eventually.
             if (flightTimer > 0f)
@@ -70,16 +86,157 @@ namespace CabinetMan.Player
             }
 
             isGrounded = GroundCheck();
+
+            xInputRaw = input.moveDirRaw.x;
+            zInputRaw = input.moveDirRaw.z;
         }
 
         private void FixedUpdate()
         {
-            PSM.currentState.FixedUpdate();
+            switch (currentState)
+            {
+                case PlayerState.IDLE:
+                    IdleFixedUpdate(); break;
+                case PlayerState.MOVING:
+                    MovingFixedUpdate(); break;
+                case PlayerState.JUMPING:
+                    JumpingFixedUpdate(); break;
+                case PlayerState.FALLING:
+                    FallingFixedUpdate(); break;
+            }
         }
 
         private bool GroundCheck()
         {
             return Physics.CheckSphere(transform.position - data.groundCheckOffset, data.groundCheckRadius, ground);
+        }
+
+        public void Move()
+        {
+            float currentSpeed = rb.velocity.magnitude;
+
+            Vector3 moveForce = new Vector3(xInputRaw, 0, zInputRaw) * data.moveForce;
+
+            //Stop applying force if player has reached their max speed
+            bool isOverMaxSpeed = currentSpeed > data.maxSpeed;
+            if (isOverMaxSpeed)
+                moveForce = Vector3.zero;
+
+            rb.AddForce(moveForce);
+        }
+
+
+        //-----States-----
+        public void IdleUpdate()
+        {
+            if (xInputRaw != 0 || zInputRaw != 0)
+                currentState = PlayerState.MOVING;
+
+            if (input.PressedJump)
+            {
+                data.jumpCount++;
+                currentState = PlayerState.JUMPING;
+            }
+        }
+
+        public void IdleFixedUpdate()
+        {
+            if (rb.velocity.magnitude > 0)
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, data.slowDownSpeed * Time.deltaTime);
+            }
+        }
+
+        public void MovingUpdate()
+        {
+            if (xInputRaw == 0 && zInputRaw == 0)
+                currentState = PlayerState.IDLE; //If no input from the player, switch to idle state
+
+            if (input.PressedJump)
+            {
+                data.jumpCount++;
+                currentState = PlayerState.JUMPING;
+            }
+        }
+
+        public void MovingFixedUpdate()
+        {
+            Move();
+        }
+
+        public void JumpingUpdate()
+        {
+            if (!input.HoldingJump)
+            {
+                jumpTimer = 0;
+                currentState = PlayerState.FALLING;
+            }
+
+            if (data.jumpCount == 2)
+            {
+                flightTimer = data.maxFlightTime;
+            }
+        }
+
+        public void JumpingFixedUpdate()
+        {
+            Move();
+
+            if (jumpTimer >= data.maxJumpFrames)
+            {
+                jumpTimer = 0;
+                currentState = PlayerState.FALLING;
+            }
+            else
+            {
+                rb.AddForce(new Vector3(0, data.jumpForce, 0));
+                jumpTimer++;
+            }
+        }
+
+        public void FallingUpdate()
+        {
+            if (input.PressedJump && canJump)
+            {
+                data.jumpCount++;
+                currentState = PlayerState.JUMPING;
+            }
+
+            if (isGrounded)
+            {
+                currentState = PlayerState.LANDED;
+            }
+
+            if (flightTimer == 0 && data.jumpCount > 2)
+            {
+                canJump = false;
+            }
+            else
+            {
+                canJump = true;
+            }
+        }
+
+        public void FallingFixedUpdate()
+        {
+            Move();
+        }
+
+        public void LandedUpdate()
+        {
+            jumpTimer = 0;
+            data.jumpCount = 0;
+            flightTimer = 0;
+            canJump = true;
+
+            if (xInputRaw != 0 || zInputRaw != 0)
+            {
+                currentState = PlayerState.MOVING;
+            }
+            else
+            {
+                currentState = PlayerState.IDLE;
+            }
         }
     }
 }
