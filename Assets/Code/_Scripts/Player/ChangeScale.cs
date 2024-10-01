583f82d834fd61f3299b7be419033e3d03fcdaf6
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-10-01 10:03:44 -0400

    Changes to ChangeScale
    Removed the code that was changing the scale per-frame as I realized that this behaves differently on different machines due to inconsistent frame-rates. The script now controls the timing of the change based on seconds.

--- Assets/Code/_Scripts/Player/ChangeScale.cs
+++ Assets/Code/_Scripts/Player/ChangeScale.cs
@@ -1,20 +1,14 @@
 using UnityEngine;
 
-/// <summary>
-/// By Mads :
-/// Currently changes the scale of the player when a trigger is activated.
-/// The method of changing this scale will likely change.
-/// </summary>
-
 public class ChangeScale : MonoBehaviour
 {
     public Vector3 humanSize = Vector3.one;
     public Vector3 bugSize = new Vector3(0.25f, 0.25f, 0.25f);
 
-    [Tooltip("The amount of frames it takes to complete the process of gradually changing from one size to the other.")]
-    public int totalScalingFrames = 200;
-    [Tooltip("Counter variable used in the update method during the scale-change process.")]
-    private int scalingFramesLeft = 0;
+    [Tooltip("The amount of time in seconds it takes to complete the process of changing from one size to the other.")]
+    public float scalingTimeSeconds = 2.0f;
+    [Tooltip("Timer variable used in the update method to control the duration of the scale-change process.")]
+    public float changeTimer = 0.0f;
     [Tooltip("Amount the scale and position will be changed each frame.")]
     private float changeIncrement;
 
@@ -31,19 +25,23 @@ public class ChangeScale : MonoBehaviour
     {
         transform.localScale = humanSize;
 
-        changeIncrement = (humanSize.x - bugSize.x) / totalScalingFrames; //only using the x value of the Vector3's since I need a single float and the values are the same
+        changeIncrement = (humanSize.x - bugSize.x) / scalingTimeSeconds; //only using the x value of the Vector3's since I need a single float and the values are the same
     }
 
     private void Update()
     {
-        if (scalingFramesLeft > 0)
+        if (changeTimer > 0.0f)
         {
-            transform.localScale += new Vector3(changeIncrement, changeIncrement, changeIncrement);
-            transform.position += new Vector3(0, changeIncrement, 0);
-            scalingFramesLeft--;
+            transform.localScale += new Vector3(changeIncrement, changeIncrement, changeIncrement) * Time.deltaTime;
+            transform.position += new Vector3(0, changeIncrement, 0) * Time.deltaTime;
+            changeTimer -= Time.deltaTime;
+        }
+        else
+        {
+            changeTimer = 0.0f;
         }
 
-        if (scalingFramesLeft == 0)
+        if (changeTimer == 0)
         {
             hasChanged = false;
         }
@@ -55,7 +53,7 @@ public class ChangeScale : MonoBehaviour
         {
             if (!hasChanged)
             {
-                scalingFramesLeft = 200;
+                changeTimer = scalingTimeSeconds;
                 changeIncrement = -changeIncrement;
 
                 if (currentSize == size.HUMAN)
