be8450047f95ea567511b5a38b29e05bb554f35d
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-10-10 09:39:40 -0400

    Playing around with the CameraControl script to try to make it smoother

--- Assets/Code/Scripts/CameraControl.cs
+++ Assets/Code/Scripts/CameraControl.cs
@@ -14,7 +14,7 @@ public class CameraControl : MonoBehaviour
 
     public rotationAxes axes = rotationAxes.MouseXAnyY;
     public float sensitivityX = 9.0f;
-    public float sentitivityY = 9.0f;
+    public float sensitivityY = 9.0f;
     //rotationAxes provides a visible variable in the editor and is tested in the if statements, set to 0 by default.
     //sensitivities are self-explanatory
 
@@ -23,8 +23,6 @@ public class CameraControl : MonoBehaviour
     //minimum and maximum values for Mathf.Clamp to use.
 
     private float yRot = 0;
-    private float xRot = 0;
-    private float delta;
     //variable for vertical rotation angle
 
     public Transform orientation; //represents the Player's orientation -- MM 10/10/24
@@ -44,7 +42,7 @@ public class CameraControl : MonoBehaviour
 
         else if (axes == rotationAxes.MouseY)
         {
-            yRot -= Input.GetAxis("Mouse Y") * sentitivityY;
+            yRot -= Input.GetAxis("Mouse Y") * sensitivityY;
             yRot = Mathf.Clamp(yRot, minimumY, maximumY);
 
             float xRot = transform.localEulerAngles.y;
@@ -54,15 +52,17 @@ public class CameraControl : MonoBehaviour
         //This reads a vertical input from the mouse, keeps it between two values, keeps horizontal rotation locked, and creates a new Vector3 for the x rotation's angle.
         else
         {
-            yRot -= Input.GetAxis("Mouse Y") * sentitivityY;
+            yRot -= Input.GetAxis("Mouse Y") * sensitivityY;
             yRot = Mathf.Clamp(yRot, minimumY, maximumY);
 
             float delta = Input.GetAxis("Mouse X") * sensitivityX;
             float xRot = transform.localEulerAngles.y + delta;
 
-            transform.localEulerAngles = new Vector3(yRot, xRot, 0);
+            //transform.localEulerAngles = new Vector3(yRot, xRot, 0); -- leaving this out for now becuase I think transform.rotation is smoother?? -- MM 10/10/24
 
-            orientation.rotation = Quaternion.Euler(0, xRot, 0);
+            transform.rotation = Quaternion.Euler(yRot, xRot, 0);
+
+            orientation.rotation = Quaternion.Euler(0, xRot, 0); //Updates the player's orientation to match camera's rotation on the y axis. -- MM 10/10/24
         }
         //This essentially takes the same idea of the else if statement above, but instead of locking horizontal rotation,
         //it reads horizontal rotation to a new float and adds that rotation to the horizontal rotation.
--- Assets/Code/Scripts/MoveCamera.cs
+++ Assets/Code/Scripts/MoveCamera.cs
@@ -6,7 +6,7 @@ public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition; //referencing the CameraPosition GameObject that is a child of the Player
 
-    private void Update()
+    private void LateUpdate()
     {
         transform.position = cameraPosition.position;
     }
