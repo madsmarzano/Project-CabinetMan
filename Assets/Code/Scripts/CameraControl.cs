5a93d66138e87577a58d0460bcc7c0daf5fd7884
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-10-12 11:11:29 -0400

    Adjusted CameraControl to work with the MouseX and MouseY settings
    Also gave the player transform a variable name to distinguish it from the camera's transform.

--- Assets/Code/Scripts/CameraControl.cs
+++ Assets/Code/Scripts/CameraControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    public Transform _camera; //Used to control the rotation of the camera, which is a child object of the Player -- MM 10/10/24
+    private Transform _player; //Gets transform of the Player, mainly to distinguish it from the camera transform -- MM 10/12/24
+
     public enum rotationAxes
     {
         MouseXAnyY = 0,
@@ -22,22 +25,29 @@ public class CameraControl : MonoBehaviour
     public float maximumY = 45.0f;
     //minimum and maximum values for Mathf.Clamp to use.
 
-    private float yRot = 0;
-    //variable for vertical rotation angle
+    private float yRot = 0; //variable for vertical rotation angle
+    private float xRot = 0; 
+    private float delta = 0;
 
-    public Transform _camera; //used to control the rotation of the camera, which is a child object of the Player -- MM 10/10/24
+    private void Awake()
+    {
+        _player = GetComponent<Transform>();
+    }
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        
+        Cursor.lockState = CursorLockMode.Locked; //Locks the cursor in the middle of the screen -- MM 10/12/24       
     }
 
     void LateUpdate()
     {
         if (axes == rotationAxes.MouseX)
         {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            //transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            delta = Input.GetAxis("Mouse X") * sensitivityX;
+
+            _player.rotation = Quaternion.Euler(0, xRot, 0);
+            _camera.rotation = Quaternion.Euler(0, xRot, 0);
         }
         //This one simply rotates the camera horizontally if the mouse is moved as such.
 
@@ -46,24 +56,24 @@ public class CameraControl : MonoBehaviour
             yRot -= Input.GetAxis("Mouse Y") * sensitivityY;
             yRot = Mathf.Clamp(yRot, minimumY, maximumY);
 
-            float xRot = transform.localEulerAngles.y;
+            xRot = transform.localEulerAngles.y;
 
-            transform.localEulerAngles = new Vector3(yRot, xRot, 0);
+            _player.localEulerAngles = new Vector3(yRot, xRot, 0);
         }
         //This reads a vertical input from the mouse, keeps it between two values, keeps horizontal rotation locked, and creates a new Vector3 for the x rotation's angle.
+
         else
         {
             yRot -= Input.GetAxis("Mouse Y") * sensitivityY;
             yRot = Mathf.Clamp(yRot, minimumY, maximumY);
 
-            float delta = Input.GetAxis("Mouse X") * sensitivityX;
-            float xRot = transform.localEulerAngles.y + delta;
+            delta = Input.GetAxis("Mouse X") * sensitivityX;
+            xRot = transform.localEulerAngles.y + delta;
 
             //transform.localEulerAngles = new Vector3(yRot, xRot, 0); //-- leaving this out for now becuase I think transform.rotation is smoother?? -- MM 10/10/24
 
+            _player.rotation = Quaternion.Euler(0, xRot, 0); //Rotates the player on the y axis only. -- MM 10/10/24
             _camera.rotation = Quaternion.Euler(yRot, xRot, 0); //Rotates the camera. -- MM 10/10/24
-
-            transform.rotation = Quaternion.Euler(0, xRot, 0); //Rotates the player on the y axis only. -- MM 10/10/24
 
         }
         //This essentially takes the same idea of the else if statement above, but instead of locking horizontal rotation,
