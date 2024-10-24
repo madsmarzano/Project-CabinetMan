64358b2662dd0f0c05325c7bb7be74ad70b6765d
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-10-24 09:48:29 -0400

    Basic setup of vent-specific player logic
    - Added some bools in the Player script to enable/disable actions like jumping and changing scale.
    - Created an isInVent bool that eventually I will have evaluate to true whenever the Player enters a vent scene.
    - Started creating vent-specific player states that inherit from the PlayerVentState parent class. This class will handle the deactivation of abilities.
    - Added some logic to the ChangeScale class that intializes the player to a size in the Start method based on checking the isInVent bool. If true, player starts as a bug, else player starts as a human.

--- Assets/Code/Scripts/Player/Player.cs
+++ Assets/Code/Scripts/Player/Player.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// By Mads.
+/// This script is the core of the Player controller. It uses a state machine to change between states such as Idle, Moving, Jumping, etc.
+/// </summary>
+
 public class Player : MonoBehaviour
 {
     [HideInInspector]
@@ -10,8 +15,6 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     [HideInInspector]
     public CapsuleCollider cc;
-
-    public Transform orientation; //A child GameObject of the player that updates the direction the player is facing based on where the camera is facing.
 
     [HideInInspector]
     public PlayerData data; // This is the main instance of PlayerData that is referenced.
@@ -22,10 +25,16 @@ public class Player : MonoBehaviour
 
     [Header("Jumping and Movement")]
     public bool isGrounded;
-    //public LayerMask ground; -- trying a different approach, I think this will be too confusing
-
+    //public LayerMask ground; -- trying a different approach, I think this will be too confusing (MM 10/23/24)
     public float jumpTimer = 0;
+    public float flightTimer = 0f; //Amount of the time the player can spend flying. 
+
+    [Header("Player Abilities")]
     public bool canJump = true;
+    public bool canChangeSize = true;
+
+    [Header("Vent Stuff")]
+    public bool isInVent = false;
 
     public PlayerStateMachine stateMachine;
 
@@ -38,8 +47,6 @@ public class Player : MonoBehaviour
     public PlayerLandedState landedState;
 
     #endregion
-
-    public float flightTimer = 0f;
 
     private void Awake()
     {
