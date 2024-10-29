4169af5d8d85f5df432fef1638c26b652fa7d9ff
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-10-29 09:34:31 -0400

    Setup North Vent Passage in Clothing Store
    Working teleporter script

---
+++ Assets/Code/Scripts/Teleporter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// By Mads:
+/// 
+/// Objects that trigger teleportation will have a trigger collider attached to them.
+/// When the player collides with the trigger, their position is updated to the teleportPoint position. 
+/// Currently only works when Player is bug-size.
+/// 
+/// </summary>
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform teleportPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //check if the player is bug-size 
+            //if yes, proceed with teleportation
+            ChangeScale sizeCheck = other.GetComponent<ChangeScale>();
+            if (sizeCheck.currentSize == ChangeScale.size.BUG)
+            {
+                other.transform.position = teleportPoint.position;
+            }
+        }
+    }
+}
