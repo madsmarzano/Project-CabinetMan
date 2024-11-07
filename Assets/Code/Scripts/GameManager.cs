285c38af3472e82f794077c45a3920588036a4a9
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-11-07 12:13:38 -0500

    some more UI stuff

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -17,6 +17,11 @@ public class GameManager : MonoBehaviour
         "INSERT ROOM 5 INFO HERE"
     };
 
+    public List<Item> CurrentInventory = new List<Item>();
+    //List of all possible inventory items in the game
+    public List<Item> InventoryItems = new List<Item>();
+    //public Sprite[] inventorySprites;
+
     private void Awake()
     {
         if (instance == null)
@@ -30,6 +35,18 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);;
+    }
+
+    private void LoadItems()
+    {
+        //CDs
+        InventoryItems.Add(new Item("FoodCourtCD", 0, "CD Found in the Food Court"));
+        InventoryItems.Add(new Item("ClothingSToreCD", 1, "CD Found in the Clothing Store"));
+        InventoryItems.Add(new Item("MusicStoreCD", 2, "CD Found in the Music Store"));
+        InventoryItems.Add(new Item("VentMazeCD", 0, "CD Found in the Vent System"));
+        InventoryItems.Add(new Item("PlayplaceCD", 0, "CD Found in the Playplace"));
+
+        //Everything else
     }
 
 
