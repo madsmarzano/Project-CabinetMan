8ffec7f2e2e881c0cefd504fd35dc725a224eef7
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-11-09 21:11:50 -0500

    got items to add themselves into the inventory (game manager)

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -17,10 +17,8 @@ public class GameManager : MonoBehaviour
         "INSERT ROOM 5 INFO HERE"
     };
 
-    public List<Item> CurrentInventory = new List<Item>();
-    //List of all possible inventory items in the game
-    public List<Item> InventoryItems = new List<Item>();
-    //public Sprite[] inventorySprites;
+    //List to store the items that are in the Player's inventory
+    public List <InventoryItem> Inventory;
 
     private void Awake()
     {
@@ -35,18 +33,6 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);;
-    }
-
-    private void LoadItems()
-    {
-        //CDs
-        InventoryItems.Add(new Item("FoodCourtCD", 0, "CD Found in the Food Court"));
-        InventoryItems.Add(new Item("ClothingSToreCD", 1, "CD Found in the Clothing Store"));
-        InventoryItems.Add(new Item("MusicStoreCD", 2, "CD Found in the Music Store"));
-        InventoryItems.Add(new Item("VentMazeCD", 0, "CD Found in the Vent System"));
-        InventoryItems.Add(new Item("PlayplaceCD", 0, "CD Found in the Playplace"));
-
-        //Everything else
     }
 
 
---
+++ Assets/Code/Scripts/InventoryItem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// By Mads.
+/// Attach this script to a GameObject that will be picked up by the Player and added to their inventory.
+/// </summary>
+
+public class InventoryItem : MonoBehaviour
+{
+    public string itemname;
+    public Sprite itemIcon;
+    public bool isUsable;
+    public bool isCD;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            AddToInventory();
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void AddToInventory()
+    {
+        GameManager.instance.Inventory.Add(this);
+    }
+}
