7ae0014c4e3d4d8122800bb359986915b1a03ce3
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-11-29 23:16:21 -0500

    Update VentFan.cs

--- Assets/Code/Scripts/VentMaze/VentFan.cs
+++ Assets/Code/Scripts/VentMaze/VentFan.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class VentFan : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject _player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
